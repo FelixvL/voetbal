54556dae65fb10e1f246c93c639788baaa66f2cc
FelixvL <digitaleservice@gmail.com>
2020-02-20 13:27:13 +0100

    ff wat typen

---
+++ Demo.java
@@ -0,0 +1,15 @@
+class Demo{
+   public static void main(String[] args) {
+	   Team team1 = new Team();
+	   Team team2 = new Team();
+   }
+}
+class Team{
+	Speler keeper = new Speler();
+	Speler verdediger = new Speler();
+	Speler aanvaller = new Speler();
+	
+}
+class Speler{
+	
+}
